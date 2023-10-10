22d2fa30afcf47a3a6b5aa49b7b1063d9c9e6687
kunal00000 <kunalverma2468@gmail.com>
2023-10-11 02:17:20 +0530

    feat: create db instance

--- apps/website/src/trpc/index.ts
+++ apps/website/src/trpc/index.ts
@@ -1,9 +1,10 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
@@ -12,6 +13,21 @@ export const appRouter = router({
     }
 
     // check if the user is in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id
+      }
+    });
+
+    if (!dbUser) {
+      // create the user in the database
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email
+        }
+      });
+    }
 
     return { success: true };
   })
