d44e6582b259f03b9a1e1067d1255e5bb0258681
kunal00000 <kunalverma2468@gmail.com>
2023-10-15 02:02:35 +0530

    feat: delete file trpc

--- apps/website/src/trpc/index.ts
+++ apps/website/src/trpc/index.ts
@@ -2,6 +2,7 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { privateProcedure, publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { db } from '@/db';
+import { z } from 'zod';
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -40,7 +41,32 @@ export const appRouter = router({
         userId
       }
     });
-  })
+  }),
+
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId
+        }
+      });
+
+      if (!file) {
+        throw new TRPCError({ code: 'NOT_FOUND', message: 'File not found' });
+      }
+
+      await db.file.delete({
+        where: {
+          id: input.id
+        }
+      });
+
+      return file;
+    })
 });
 
 // Export type router type signature,
