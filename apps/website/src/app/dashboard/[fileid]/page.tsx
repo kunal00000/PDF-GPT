a18157b19dd342a23bac1b7b6bf9e1d5109b9782
kunal00000 <kunalverma2468@gmail.com>
2023-10-25 00:00:28 +0530

    feat: chat wrapper

--- apps/website/src/app/dashboard/[fileid]/page.tsx
+++ apps/website/src/app/dashboard/[fileid]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound, redirect } from 'next/navigation';
 
-import ChatWrapper from '@/components/ChatWrapper';
 import PdfRenderer from '@/components/PdfRenderer';
+import ChatWrapper from '@/components/chat/ChatWrapper';
 import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
@@ -39,7 +39,7 @@ const Page = async ({ params }: PageProps) => {
         </div>
 
         <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
-          <ChatWrapper />
+          <ChatWrapper fileId={file.id} />
         </div>
       </div>
     </div>
