f51ea17d11592be9abcf4a96ddc8d00d470638e1
kunal00000 <kunalverma2468@gmail.com>
2023-10-29 01:44:37 +0530

    feat: get file messages trpc route

--- apps/website/src/trpc/index.ts
+++ apps/website/src/trpc/index.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 
+import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
 import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { TRPCError } from '@trpc/server';
@@ -44,6 +45,61 @@ export const appRouter = router({
       },
     });
   }),
+
+  getFileMessages: privateProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+        fileId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const { userId } = ctx;
+      const { fileId, cursor } = input;
+
+      const limit = input.limit ?? INFINITE_QUERY_LIMIT;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: fileId,
+          userId,
+        },
+      });
+
+      if (!file) {
+        throw new TRPCError({ code: 'NOT_FOUND', message: 'File not found' });
+      }
+
+      const messages = await db.message.findMany({
+        where: {
+          fileId,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+        cursor: cursor ? { id: cursor } : undefined,
+        take: limit + 1,
+        select: {
+          id: true,
+          text: true,
+          isUserMessage: true,
+          createdAt: true,
+        },
+      });
+
+      let nextCursor: typeof cursor | undefined = undefined;
+
+      if (messages.length > limit) {
+        const nextItem = messages.pop();
+        nextCursor = nextItem?.id;
+      }
+
+      return {
+        messages,
+        nextCursor,
+      };
+    }),
 
   getFileUploadStatus: privateProcedure
     .input(z.object({ fileId: z.string() }))
