632077fe34e0199d2ba910dbd0016b7346a4275d
kunal00000 <kunalverma2468@gmail.com>
2023-10-31 01:35:56 +0530

    empty

--- apps/website/src/app/pricing/page.tsx
+++ apps/website/src/app/pricing/page.tsx
@@ -111,7 +111,7 @@ const Page = () => {
                     </h3>
                     <p className='text-gray-500'>{tagline}</p>
                     <p className='my-5 font-display text-6xl font-semibold'>
-                      {price}₹
+                      ₹{price}
                     </p>
                     <p className='text-gray-500'>per month</p>
                   </div>
