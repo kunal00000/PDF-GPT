6e5241f2c4fe719ecae3566e9fa1051a6c1660bc
kunal00000 <kunalverma2468@gmail.com>
2023-11-01 01:43:45 +0530

    feat: add limit for file size

--- apps/website/src/app/api/uploadthing/core.ts
+++ apps/website/src/app/api/uploadthing/core.ts
@@ -12,104 +12,118 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
 const f = createUploadthing();
 
-export const ourFileRouter = {
-  pdfUploader: f({ pdf: { maxFileSize: '4MB' } })
-    .middleware(async ({ req }) => {
-      const { getUser } = getKindeServerSession();
-      const user = getUser();
+const middleware = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
 
-      if (!user || !user.id) throw new Error(`Unauthorized`);
+  if (!user || !user.id) throw new Error(`Unauthorized`);
 
-      const subscriptionPlan = await getUserSubscriptionPlan();
+  const subscriptionPlan = await getUserSubscriptionPlan();
 
-      return { subscriptionPlan, userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      const createdFile = await db.file.create({
+  return { subscriptionPlan, userId: user.id };
+};
+
+const onUploadComplete = async ({
+  metadata,
+  file,
+}: {
+  metadata: Awaited<ReturnType<typeof middleware>>;
+  file: {
+    key: string;
+    name: string;
+    url: string;
+  };
+}) => {
+  const createdFile = await db.file.create({
+    data: {
+      key: file.key,
+      name: file.name,
+      userId: metadata.userId,
+      url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+      uploadStatus: 'PROCESSING',
+    },
+  });
+
+  try {
+    const response = await fetch(
+      `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+    );
+
+    const blob = await response.blob();
+
+    const loader = new PDFLoader(blob);
+
+    const pageLevelDocs = await loader.load();
+
+    pageLevelDocs.forEach((pageLevelDoc) => {
+      pageLevelDoc.metadata = {
+        ...pageLevelDoc.metadata,
+        fileId: createdFile.id,
+      };
+    });
+
+    const pagesAmt = pageLevelDocs.length; // pages count
+
+    const { subscriptionPlan } = metadata;
+    const { isSubscribed } = subscriptionPlan;
+    const isProExceeded =
+      pagesAmt > PLANS.find((plan) => plan.name === 'Pro')!.pagesPerPdf;
+    const isFreeExceeded =
+      pagesAmt > PLANS.find((plan) => plan.name === 'Free')!.pagesPerPdf;
+    if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)) {
+      await db.file.update({
         data: {
-          key: file.key,
-          name: file.name,
-          userId: metadata.userId,
-          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-          uploadStatus: 'PROCESSING',
+          uploadStatus: 'FAILED',
+        },
+        where: {
+          id: createdFile.id,
         },
       });
+    }
 
-      try {
-        const response = await fetch(
-          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-        );
+    // vectorize and index entire document
+    const pineconeIndex = await pinecone.Index('docuconvo');
 
-        const blob = await response.blob();
+    const embeddings = new HuggingFaceInferenceEmbeddings({
+      model: 'sentence-transformers/all-MiniLM-L6-v2',
+      apiKey: HF_TOKEN,
+    });
 
-        const loader = new PDFLoader(blob);
+    await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+      pineconeIndex,
+    });
 
-        const pageLevelDocs = await loader.load();
+    // set upload status to failed
+    await db.file.update({
+      data: {
+        uploadStatus: 'SUCCESS',
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  } catch (err) {
+    console.error(err);
 
-        pageLevelDocs.forEach((pageLevelDoc) => {
-          pageLevelDoc.metadata = {
-            ...pageLevelDoc.metadata,
-            fileId: createdFile.id,
-          };
-        });
+    // set upload status to failed
+    await db.file.update({
+      data: {
+        uploadStatus: 'FAILED',
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  }
+};
 
-        const pagesAmt = pageLevelDocs.length; // pages count
-
-        const { subscriptionPlan } = metadata;
-        const { isSubscribed } = subscriptionPlan;
-        const isProExceeded =
-          pagesAmt > PLANS.find((plan) => plan.name === 'Pro')!.pagesPerPdf;
-        const isFreeExceeded =
-          pagesAmt > PLANS.find((plan) => plan.name === 'Free')!.pagesPerPdf;
-        if (
-          (isSubscribed && isProExceeded) ||
-          (!isSubscribed && isFreeExceeded)
-        ) {
-          await db.file.update({
-            data: {
-              uploadStatus: 'FAILED',
-            },
-            where: {
-              id: createdFile.id,
-            },
-          });
-        }
-
-        // vectorize and index entire document
-        const pineconeIndex = await pinecone.Index('docuconvo');
-
-        const embeddings = new HuggingFaceInferenceEmbeddings({
-          model: 'sentence-transformers/all-MiniLM-L6-v2',
-          apiKey: HF_TOKEN,
-        });
-
-        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
-          pineconeIndex,
-        });
-
-        // set upload status to failed
-        await db.file.update({
-          data: {
-            uploadStatus: 'SUCCESS',
-          },
-          where: {
-            id: createdFile.id,
-          },
-        });
-      } catch (err) {
-        console.error(err);
-
-        // set upload status to failed
-        await db.file.update({
-          data: {
-            uploadStatus: 'FAILED',
-          },
-          where: {
-            id: createdFile.id,
-          },
-        });
-      }
-    }),
+export const ourFileRouter = {
+  freePlanUploader: f({ pdf: { maxFileSize: '4MB' } })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
+  proPlanUploader: f({ pdf: { maxFileSize: '16MB' } })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
